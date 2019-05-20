f503249fb81d19f0cf88c21d18f45d17eebaf981
丨Rainbow丨 <775183940@qq.com>
2019-05-20 15:51:24 +0800

    完结完结

---
+++ app/src/main/java/com/sdr/sdrlib/ui/SDRHikVisionActivity.java
@@ -0,0 +1,43 @@
+package com.sdr.sdrlib.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.sdr.sdrlib.R;
+import com.sdr.sdrlib.base.BaseActivity;
+import com.sdr.sdrlib.common.AppItemRecyclerAdapter;
+import com.sdr.sdrlib.common.MainItem;
+
+import butterknife.BindView;
+
+/**
+ * Created by HyFun on 2019/05/16.
+ * Email: 775183940@qq.com
+ * Description:
+ */
+
+public class SDRHikVisionActivity extends BaseActivity {
+    @BindView(R.id.main_recycler_view)
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("SDRHikVisionActivity");
+        setDisplayHomeAsUpEnabled();
+
+        AppItemRecyclerAdapter adapter = AppItemRecyclerAdapter.setAdapter(recyclerView);
+
+        //
+        adapter.addData(new MainItem("查看视频", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        }));
+
+    }
+}
